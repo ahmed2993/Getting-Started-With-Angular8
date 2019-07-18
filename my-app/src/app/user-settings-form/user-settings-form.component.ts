22f0435ab3af3d4a675567c6acf5bab464b0aaee
Mumin Quadri <mumin.quadri25@gmail.com>
2019-07-19 02:32:32 +0530

    Add Form Validation

--- my-app/src/app/user-settings-form/user-settings-form.component.ts
+++ my-app/src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -18,6 +19,12 @@ export class UserSettingsFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  onBlur(field: NgModel) {
+    console.log('in onBlur: ', field.valid);
+  }
+  onSubmit(form: NgForm) {
+    console.log('in onSubmit: ', form.valid);
   }
 
 }
